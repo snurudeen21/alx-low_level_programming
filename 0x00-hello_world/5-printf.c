219facddad73fe472333b46d9e508d5010d930b8
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-07 19:42:32 -0700

    print!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the string in the system defined function: printf
+ * The program displays: with proper grammar, but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
