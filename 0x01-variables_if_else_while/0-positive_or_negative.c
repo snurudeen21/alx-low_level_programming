d9f065ba029993b3920efdb5c9abdb2710f93588
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-09 13:20:15 -0700

    if, else statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* main - select an integer at random and check if n=0 or n<0 or n>1 using the fun ction: printf to printout messages
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+{
+	printf("%d is positive\n", n);
+}
+	else if (n==0)
+{
+	printf("%d is zero\n", n);
+}
+	else
+	printf("%d is negative\n", n);
+
+	return (0);
+}
