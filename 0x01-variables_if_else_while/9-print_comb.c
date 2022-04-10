f2d41c28d92aa0b2e15b480a5763e4ea911d87bb
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-10 01:33:49 -0700

    0 - 9 with formatting by putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio>
+/**
+ * main - print 0-9 with formatting using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+{
+	putchar (i + '0');
+	if (i != 9)
+{
+	putchar ('0' - 4);
+	putchar (' ');
+}
+	i++;
+}
+	putchar ('\n');
+	return (0);
+}
