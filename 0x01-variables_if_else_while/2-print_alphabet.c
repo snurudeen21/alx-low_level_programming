ca1a7ad3674c33ae27bc0039afec6f07830fb535
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-09 23:20:41 -0700

    while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- print letters from a - z using putchar function
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+{
+	putchar (ch);
+}
+	return (0);
+}
