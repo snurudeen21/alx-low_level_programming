9588263fd21d2bb453046416d6435dfb0cb826e0
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 08:17:00 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _islower -Return 1 if char is lower case else return 0
+ * _islower -Return 1 if char is lower case else retrn 0
  * Return:0
  */
 int _islower(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c > 'a' && c < 'z')
 {
 	return (1);
 }
