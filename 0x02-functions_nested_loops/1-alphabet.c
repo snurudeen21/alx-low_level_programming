d89a2937d9c3e7c1e91e7647538ef9836f88ac00
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 07:01:41 -0700

    a - z by function:_putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * main - print a - z using functio: _putchar
+ * print_alphabet - print a - z using functio: _putchar
  * Return: void
  */
 void print_alphabet(void)
 {
 	char ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 {
 	_putchar(ch);
