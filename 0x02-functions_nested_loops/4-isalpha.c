7f9f853c4df9f2f28639a136692cc478cee19e7c
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 14:49:09 -0700

    upper and lower case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - return 1 if char is upper or lower case else return 0
+ * @c: will hold the variables to be checked
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
