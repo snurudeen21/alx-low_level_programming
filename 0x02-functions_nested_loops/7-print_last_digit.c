585a066b9e50af0fba1d62638af3f09f5512662b
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 15:28:44 -0700

    modulo of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: holds the variable containing last digit
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int m = n % 10;
+	return (m);
+}
