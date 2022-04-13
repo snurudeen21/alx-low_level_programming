2344b25a8a1ee2ed9e5548988d8aa4e20958f205
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 00:14:25 -0700

    functions and nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int _putchar (char c);
 	_putchar ('_');
-	_putchar ('_');
-	_putchar ('_');
-	_putchar ('_');
-	_putchar ('_');
-	_putchar ('_');
-	_putchar ('_');
-	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
 }
 	int _putchar (char c)
 {
