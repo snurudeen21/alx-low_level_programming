43d6c5f65a73f719f70e6037aaeebb3e867e895a
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 15:20:03 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,39 +5,18 @@
  *@n: The variable to check
  *Return: Always 0.
  */
-
- int _abs(int n)
-
+int _abs(int n)
 {
-
 	if (n < 0)
-
-	{
-
-		return (n * (-1));
-
-	}
-
-
-
+{
+	return (n * (-1));
+}
 	else if (n == 0)
-
-	{
-
-		return (0);
-
-	}
-
-
-
+{
+	return (0);
+}
 	else
-
-	{
-
-		return (n);
-
-	}
-
-
-
+{
+	return (n);
+}
 }
