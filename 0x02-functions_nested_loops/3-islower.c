faba3fa3b85d5a34c1808a9deb695315af957b05
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 07:51:55 -0700

    lower case by _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower -Return 1 if char is lower case else return 0
+ * Return:0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
