b4cd6dae4677cdfd1ea66239e1b037f2b3656513
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-04-13 04:54:30 -0700

    function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,22 +4,18 @@
  * main - print _putchar using functions
  * Return: 0
  */
-	int main(void)
+	void _putchar (char c[])
 {
-	int _putchar (char c);
-	int i;
-	char ch[i];
-	_putchar (ch[i]);
-	putchar ('\n');
-	return (0);
-}
-	int _putchar (char c)
-{
-	char ch[9] = "_putchar";
 	int i = 0;
-	while (ch[i] != '\0')
+	while (i < 8)
 {
-	putchar (ch[i]);
+	putchar (c[i]);
 	i++;
 }
+}
+	int main (void)
+{
+	_putchar (c);
+	putchar ('\n');
+	return (0);
 }
