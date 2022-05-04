b3acf6f3078032dad46218ff4a13d719f3857207
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-05-04 09:01:09 -0700

    C static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,34 +1,25 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _strstr - first occurrence of the substring needle in the string haystack
- * @haystack: main str to be examined
- * @needle: searched in haystack
- * Return: return 0
- */
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
-
-
-char  *_strstr(char *haystack, char *needle)
-
-{
-	char *str1, *str2; /*Declaring variables*/
-
-	while (*haystack != '\0')
-{
-	str1 = haystack; /*values*/
-	str2 = needle;
-
-      /*Star WHILE*/
-	while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
-{
-	haystack++;
-	str2++;
-}
-	if (*str2 == '\0')
-	return (str1);
-	haystack = str1 + 1;
-}
-	return (0);
-
-}
+#endif
