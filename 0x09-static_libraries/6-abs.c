bec343b1e26652ed2ff819fe3d2c92d0a51b3776
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-05-04 08:05:46 -0700

    static libraries in C

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_abs - return value absolute
+ *@n: The variable to check
+ *Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+{
+	return (n * (-1));
+}
+	else if (n == 0)
+{
+	return (0);
+}
+	else
+{
+	return (n);
+}
+}
