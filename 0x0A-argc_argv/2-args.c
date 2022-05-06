384c49b0f854ff2c388fe4866da160fb97a832ad
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-05-06 02:38:12 -0700

    List the argument to a program

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print the arguments to the program
+ * @argc: Counts arguments
+ * @argv: Arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+}
+	return (0);
+}
