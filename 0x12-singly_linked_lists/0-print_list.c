701641db72687e0e06ce634646f2fe2b0fecda5a
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-05-26 01:09:26 -0700

    Singly Linked List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,21 @@
 
 size_t print_list(const list_t *h)
 {
-size_t nelem;
-nelem = 0;
+	size_t nelem;
 
-while (h != NULL)
+	nelem = 0;
+
+	while (h != NULL)
 {
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
-else
+	if (h->str == NULL)
+	printf("[%d] %s\n", 0, "(nil)");
 
-printf("[%d] %s\n", h->len, h->str);
+	else
+	printf("[%d] %s\n", h->len, h->str);
 
-h = h->next;
+	h = h->next;
 
-nelem++;
+	nelem++;
 }
-return (nelem);
+	return (nelem);
 }
