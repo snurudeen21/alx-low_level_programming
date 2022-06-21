f24a05bf858131c249905b1e5d3934eacf97f754
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-06-21 14:49:51 -0700

    malloc calloc realloc by C

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- *is_digit - checls if a string contains a non-digit char.
- *@s: string to be evaluated
- *Return: 0 if a non-digit is foud, 1 otherwise.
+ * _putchar - prints a character
+ * @c: character to print
+ * Return: 1 on success
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * is_digit - checls if a string contains a non-digit char.
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is foud, 1 otherwise.
  */
 
 int is_digit(char *s)
@@ -22,9 +34,9 @@ int is_digit(char *s)
 }
 
 /**
- *_strlen - returns the length of a string
- *@s: string to evaluate
- *Return: length of the string
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: length of the string
  */
 
 int _strlen(char *s)
@@ -60,7 +72,7 @@ int main(int argc, char *argv[])
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1 = argv[1], s2 = argv[s];
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 	errors();
 	len1 = _strlen(s1);
