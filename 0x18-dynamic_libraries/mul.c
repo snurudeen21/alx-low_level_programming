5c668250ad0f934b1a772348c0e9c01e948e9543
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-03 02:17:02 +0100

    mul__a__b

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *_mul - multiplication of a & b.
+ *@a: first number
+ *@b: second number
+ *Return: mul of a & b.
+ */
+
+int _mul(int a, int b)
+{
+    int mult;
+    mult = a + b;
+    return (mult);
+}
