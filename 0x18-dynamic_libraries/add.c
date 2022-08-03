d0767c60a01425dd831f769aa5540f451a06d75c
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-03 02:13:08 +0100

    add__a__b

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *_add - addition of a & b.
+ *@a: first number
+ *@b: second number
+ *Return: sum of a & b.
+ */
+
+int _add(int a, int b)
+{
+    int sum;
+    sum = a + b;
+    return (sum);
+}
