4590089793429b18c324807875a1d6a78dcfb772
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-03 02:21:38 +0100

    mod__a__b

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *_mod - modulus of a & b.
+ *@a: first number
+ *@b: second number
+ *Return: modu of a & b.
+ */
+
+int _modu(int a, int b)
+{
+    int modu;
+    modu = a % b;
+    return (modu);
+}
