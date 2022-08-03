09b974ede54ba565a212f4a1f8648626dd268a05
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-03 02:15:27 +0100

    sub__a__b

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *_sub - subtraction of a & b.
+ *@a: first number
+ *@b: second number
+ *Return: sub of a & b.
+ */
+
+int _sub(int a, int b)
+{
+    int subt;
+    subt = a - b;
+    return (subt);
+}
