46268f80c12d02955c0c033071669bf63e0fb064
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-05 01:50:46 +0100

    div_a_b

--- 0x18-dynamic_libraries/div.c
+++ 0x18-dynamic_libraries/div.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *_div - division of a & b.
+ *div - division of a & b.
  *@a: first number
  *@b: second number
  *Return: div of a & b.
  */
 
-int _div(int a, int b)
+int div(int a, int b)
 {
 	int div;
 
