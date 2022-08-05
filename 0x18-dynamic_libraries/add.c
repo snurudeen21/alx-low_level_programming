0b70685e7fbe2927f81a17bbeb8f3324035625e0
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-05 01:49:57 +0100

    add_a_b

--- 0x18-dynamic_libraries/add.c
+++ 0x18-dynamic_libraries/add.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *_add - addition of a & b.
+ *add - addition of a & b.
  *@a: first number
  *@b: second number
  *Return: sum of a & b.
  */
 
-int _add(int a, int b)
+int add(int a, int b)
 {
 	int sum;
 
