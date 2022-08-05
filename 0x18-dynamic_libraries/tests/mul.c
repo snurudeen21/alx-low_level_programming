d3e546586fa18ba49c48b93d1d6fdb8af1f5a111
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-08-04 18:10:19 -0700

    Newbies

---
+++ 0x18-dynamic_libraries/tests/mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *mul - multiplication of a & b.
+ *@a: first number
+ *@b: second number
+ *Return: mul of a & b.
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
