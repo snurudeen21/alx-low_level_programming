0cfdea64ba84fde5708f484e6b33a170621688c0
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-07-18 00:49:44 +0100

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Prototypes */
+/* Task 0. Linear search */
+int linear_search(int *array, size_t size, int value);
+
+/* Task 1. Binary search */
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
